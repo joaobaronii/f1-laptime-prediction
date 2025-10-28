7d8f238d3aaeaeba2eba461b08a99cdc13a3b0f7
joaobaronii <joaocbaroni7@gmail.com>
2025-10-27 22:53:29 -0300

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import mlflow
 
 drivers_numbers = {
     "VER": 1,
@@ -23,8 +24,53 @@ drivers_numbers = {
     "BEA": 87,
 }
 
+model_version = {
+    "silverstone": 1,
+    "monza": 2,
+    "arabia": 3,
+    "canada": 4,
+    "monaco": 5,
+}
+
+drivers = [
+    "VER",
+    "NOR",
+    "BOR",
+    "HAD",
+    "GAS",
+    "ANT",
+    "ALO",
+    "LEC",
+    "STR",
+    "TSU",
+    "ALB",
+    "HUL",
+    "LAW",
+    "OCO",
+    "COL",
+    "HAM",
+    "SAI",
+    "RUS",
+    "PIA",
+    "BEA",
+]
+
+gps = ["silverstone", "monaco", "monza", "arabia", "canada"]
+
+mlflow.set_tracking_uri("http://localhost:5000")
+
+
 st.set_page_config(page_title="F1 Prediction", page_icon="🏎️", layout="wide")
 
 st.markdown("""
 # F1 Timelap Prediction App
 """)
+
+col1, col2 = st.columns(2)
+
+with col1.container(border=True):
+    gp = st.selectbox("GP", gps)
+
+
+with col2.container(border=True):
+    driver = st.selectbox("Driver", drivers)
