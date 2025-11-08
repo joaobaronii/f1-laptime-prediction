5b5543c68836a78260de0b04a6557359efcefa04
joaobaronii <joaocbaroni7@gmail.com>
2025-11-08 20:11:14 -0300

    feat inputs

--- app.py
+++ app.py
@@ -55,6 +55,19 @@ drivers = [
     "BEA",
 ]
 
+teams = [
+    "Red Bull Racing",
+    "Aston Martin",
+    "Kick Sauber",
+    "Mercedes",
+    "Williams",
+    "McLaren",
+    "Racing Bulls",
+    "Alpine",
+    "Ferrari",
+    "Haas F1 Team",
+]
+
 gps = ["silverstone", "monaco", "monza", "arabia", "canada"]
 
 mlflow.set_tracking_uri("http://localhost:5000")
@@ -63,14 +76,44 @@ mlflow.set_tracking_uri("http://localhost:5000")
 st.set_page_config(page_title="F1 Prediction", page_icon="🏎️", layout="wide")
 
 st.markdown("""
-# F1 Timelap Prediction App
+# F1 2025 Laptime Prediction App
 """)
 
-col1, col2 = st.columns(2)
-
-with col1.container(border=True):
+with st.container():
     gp = st.selectbox("GP", gps)
 
+col11, col12 = st.columns(2)
 
-with col2.container(border=True):
+with col11.container(border=True):
     driver = st.selectbox("Driver", drivers)
+
+with col12.container(border=True):
+    team = st.selectbox("Team/Car", teams)
+
+
+col21, col22 = st.columns(2)
+
+with col21.container(border=True):
+    compound = st.selectbox("Compound", ["HARD", "MEDIUM", "SOFT"])
+    
+with col22.container(border=True):
+    tyrelife = st.number_input("Tyrelife", min_value=2.0, max_value=20.0, step=1.0)
+
+
+col31, col32, col33, col34 = st.columns(4)
+
+with col31.container(border=True):
+    speedi1 = st.number_input("SpeedI1", min_value=200.0, max_value=350.0, step=1.0)
+
+with col32.container(border=True):
+    speedi2 = st.number_input("SpeedI2", min_value=200.0, max_value=350.0, step=1.0)
+
+with col33.container(border=True):
+    speedfl = st.number_input("SpeedFL", min_value=200.0, max_value=350.0, step=1.0)
+
+with col34.container(border=True):
+    speedst = st.number_input("SpeedST", min_value=200.0, max_value=350.0, step=1.0)
+
+meanspeed = (speedi1 + speedi2 + speedfl + speedst) / 4
+
+
